b504e8d8ae126f6f0e374a899b74cde5cdd888b9
DavidMartinezTrigo <davidmtrigo@hotmail.es>
2021-01-11 15:26:25 +0100

    Aplicación para determinar si un número es primo terminada

---
+++ 04_determinarSiUnNumeroEsPrimo/js/eventos.js
@@ -0,0 +1,20 @@
+let input = document.querySelector('input');
+let span = document.querySelector('span');
+let strong = document.querySelector('strong');
+
+input.value = '';
+input.focus();
+input.addEventListener('input', capturarInput);
+
+
+function capturarInput(event) {
+    span.innerText = '';
+    if (input.value > 0) {
+        (numeroPrimo(input.value) == false) ? strong.innerText = input.value + ' NO ' : strong.innerText = input.value + ' SÍ ';
+    } else {
+        input.value = '';
+        input.focus();
+        span.innerText = 'Te voy a decir si el número que buscas';
+        strong.innerText = '';
+    }
+};
---
+++ 04_determinarSiUnNumeroEsPrimo/js/funciones.js
@@ -0,0 +1,5 @@
+function numeroPrimo(pNum) {
+    for (let i = 2; i < pNum; i++) {
+        if (pNum % i == 0) return false
+    }
+}
