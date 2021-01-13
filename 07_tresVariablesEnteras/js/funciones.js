a9479a5f9a0f67d6e9cbd64e4bed3d57e531678b
DavidMartinezTrigo <davidmtrigo@hotmail.es>
2021-01-13 17:57:38 +0100

    Aplicación para comparar tres variables enteras TERMINADA

---
+++ 07_tresVariablesEnteras/js/funciones.js
@@ -0,0 +1,27 @@
+function cero(v1, v2, v3) {
+    if (v1 == 0, v2 == 0, v3 == 0) return true;
+};
+
+function positivas(v1, v2, v3) {
+    if (v1 > 0 && v2 > 0 && v3 > 0) return true;
+};
+
+function mismoSigno(v1, v2, v3) {
+    if (v1 >= 0 && v2 >= 0 && v3 >= 0 || v1 <= 0 && v2 <= 0 && v3 <= 0) return true;
+};
+
+function todosSusValoresDistintos(v1, v2, v3) {
+    if (v1 != v2 && v1 != v3 && v2 != v3) return true;
+};
+
+function dosValoresCoinciden(v1, v2, v3) {
+    if (v1 == v2 || v1 == v3 || v2 == v3) return true;
+};
+
+function maximoDosValoresCoinciden(v1, v2, v3) {
+    if ((v1 == v2 && v1 != v3) || (v1 == v3 && v1 != v2) || (v2 == v3 && v1 != v2)) return true;
+};
+
+function v2ComprendidoEntrev1Yv3(v1, v2, v3) {
+    if (((v1 > v2 && v2 > v3) || (v3 > v2 && v2 > v1))) return true
+};
