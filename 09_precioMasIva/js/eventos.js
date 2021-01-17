14f76248c1ec800bddd2a2f7f15ff4a67bcb206e
DavidMartinezTrigo <davidmtrigo@hotmail.es>
2021-01-17 08:47:09 +0100

    Aplicación precio + IVA TERMINADA

---
+++ 09_precioMasIva/js/eventos.js
@@ -0,0 +1,23 @@
+let inputs = document.getElementsByTagName('input');
+let boton = document.querySelector('a');
+let li = document.querySelector('li');
+
+for (const input of inputs) {
+    input.value = '';
+};
+
+inputs[0].focus();
+
+boton.addEventListener('click', capturarBoton);
+
+function capturarBoton(event) {
+    event.preventDefault();
+    for (const input of inputs) {
+        if (isNaN(input.value)) {
+            alert('Tienes que introducir el valor numérico');
+            input.value = '';
+            input.focus();
+        };
+    };
+    li.innerText = 'El precio total a pagar es: ' + precioMasIva(inputs[0].value, inputs[1].value) + ' euros';
+};
