2dfbf6c9b083405cb1aef6723986fc2a3b030b87
DavidMartinezTrigo <davidmtrigo@hotmail.es>
2021-01-27 09:32:31 +0100

    Depurada aplicación 7

--- 07_tresVariablesEnteras/js/eventos.js
+++ 07_tresVariablesEnteras/js/eventos.js
@@ -20,58 +20,67 @@ a.addEventListener('click', capturarInputs);
 function capturarInputs(event) {
     //Prevenir la acción por defecto(para los <a></a> y los <input type="submit"> de los formularios)
     event.preventDefault();
-    //Texto del primer li
-    let texto1 = ' las variables son cero';
-    if (cero(v1.value, v2.value, v3.value)) {
-        lis[0].innerText = 'Todas' + texto1;
+
+    if (!isNaN(v1.value) && !isNaN(v2.value) && !isNaN(v3.value)) {
+        //Texto del primer li
+        let texto1 = ' las variables son cero';
+        if (cero(v1.value, v2.value, v3.value)) {
+            lis[0].innerText = 'Todas' + texto1;
+        } else {
+            lis[0].innerText = 'NO todas' + texto1;
+        };
+        //Texto del segundo li
+        let texto2 = ' las variables son positivas';
+        if (positivas(v1.value, v2.value, v3.value)) {
+            lis[1].innerText = 'Todas' + texto2;
+        } else {
+            lis[1].innerText = 'NO todas' + texto2;
+        };
+        //Texto del tercer li
+        let texto3 = ' las variables tienen el mismo signo';
+        if (mismoSigno(v1.value, v2.value, v3.value)) {
+            lis[2].innerText = 'Todas' + texto3;
+        } else {
+            lis[2].innerText = 'NO todas' + texto3;
+        };
+        //Texto del cuarto li
+        let texto4 = ' sus valores son distintos';
+        if (todosSusValoresDistintos(v1.value, v2.value, v3.value)) {
+            lis[3].innerText = 'Todos' + texto4;
+        } else {
+            lis[3].innerText = 'NO todos' + texto4;
+        };
+        //Texto del quinto li
+        let texto5 = 'Dos de sus valores ';
+        if (dosValoresCoinciden(v1.value, v2.value, v3.value)) {
+            lis[4].innerText = texto5 + 'coinciden';
+        } else {
+            lis[4].innerText = texto5 + 'NO coinciden';
+        };
+        //Texto del sexto li
+        let texto6 = 'Como máximo dos de sus valores ';
+        if (maximoDosValoresCoinciden(v1.value, v2.value, v3.value)) {
+            lis[5].innerText = texto6 + 'coinciden';
+        } else {
+            lis[5].innerText = texto6 + 'NO coinciden';
+        };
+        //Texto del septimo li
+        let texto7 = 'El valor de la variable 2 ';
+        let texto8 = 'está comprendido entre los valores de las variables 1 y 3';
+        if (v2ComprendidoEntrev1Yv3(v1.value, v2.value, v3.value)) {
+            lis[6].innerText = texto7 + texto8;
+        } else {
+            lis[6].innerText = texto7 + 'NO ' + texto8;
+        };
+        //Mostrar los estilos de los lis
+        for (let li of lis) {
+            li.style.listStyleType = 'lower-latin';
+        };
     } else {
-        lis[0].innerText = 'NO todas' + texto1;
+        alert('¡¡NO PUEDES INTRODUCIR TEXTO!!. Si usas decimales tienes que separar con el símbolo  "."');
     };
-    //Texto del segundo li
-    let texto2 = ' las variables son positivas';
-    if (positivas(v1.value, v2.value, v3.value)) {
-        lis[1].innerText = 'Todas' + texto2;
-    } else {
-        lis[1].innerText = 'NO todas' + texto2;
+    if (v1.value == '' || v2.value == '' || v3.value == '') {
+        alert('No puede haber campos vacíos');
     };
-    //Texto del tercer li
-    let texto3 = ' las variables tienen el mismo signo';
-    if (mismoSigno(v1.value, v2.value, v3.value)) {
-        lis[2].innerText = 'Todas' + texto3;
-    } else {
-        lis[2].innerText = 'NO todas' + texto3;
-    };
-    //Texto del cuarto li
-    let texto4 = ' sus valores son distintos';
-    if (todosSusValoresDistintos(v1.value, v2.value, v3.value)) {
-        lis[3].innerText = 'Todos' + texto4;
-    } else {
-        lis[3].innerText = 'NO todos' + texto4;
-    };
-    //Texto del quinto li
-    let texto5 = 'Dos de sus valores ';
-    if (dosValoresCoinciden(v1.value, v2.value, v3.value)) {
-        lis[4].innerText = texto5 + 'coinciden';
-    } else {
-        lis[4].innerText = texto5 + 'NO coinciden';
-    };
-    //Texto del sexto li
-    let texto6 = 'Como máximo dos de sus valores ';
-    if (maximoDosValoresCoinciden(v1.value, v2.value, v3.value)) {
-        lis[5].innerText = texto6 + 'coinciden';
-    } else {
-        lis[5].innerText = texto6 + 'NO coinciden';
-    };
-    //Texto del septimo li
-    let texto7 = 'El valor de la variable 2 ';
-    let texto8 = 'está comprendido entre los valores de las variables 1 y 3';
-    if (v2ComprendidoEntrev1Yv3(v1.value, v2.value, v3.value)) {
-        lis[6].innerText = texto7 + texto8;
-    } else {
-        lis[6].innerText = texto7 + 'NO ' + texto8;
-    };
-    //Mostrar los estilos de los lis
-    for (let li of lis) {
-        li.style.listStyleType = 'lower-latin';
-    };
+
 };
--- 07_tresVariablesEnteras/js/funciones.js
+++ 07_tresVariablesEnteras/js/funciones.js
@@ -1,5 +1,5 @@
 function cero(v1, v2, v3) {
-    if (v1 == 0, v2 == 0, v3 == 0) return true;
+    if (v1 == 0 && v2 == 0 && v3 == 0) return true;
 };
 
 function positivas(v1, v2, v3) {
@@ -7,7 +7,7 @@ function positivas(v1, v2, v3) {
 };
 
 function mismoSigno(v1, v2, v3) {
-    if (v1 >= 0 && v2 >= 0 && v3 >= 0 || v1 <= 0 && v2 <= 0 && v3 <= 0) return true;
+    if (v1 > 0 && v2 > 0 && v3 > 0 || v1 < 0 && v2 < 0 && v3 < 0) return true;
 };
 
 function todosSusValoresDistintos(v1, v2, v3) {
